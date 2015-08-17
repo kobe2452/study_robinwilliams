c43543d2451f57f41a19fa16d059081a831c4855
kobe2452 <tongliu0314@gmail.com>
2015-08-17 16:24:16 -0400

    add functions to extract tweets from month folders

--- datasift2json.py
+++ datasift2json.py
@@ -2,8 +2,8 @@ import os, timeit, math
 import ujson as json
 from collections import defaultdict
 
+def export_DataSift_file(file_path):
 
-def exportDataSift(file_path):
     print "Now processing DataSift file: " + file_path
     
     jsonfile = open(file_path, "r")
@@ -17,61 +17,73 @@ def exportDataSift(file_path):
     tweet_list = []
         
     for each in interactions:
-
         twitter = each['twitter']
-
-        # if 'twitter' in each:
-        #     # content = each['demographic']
-        #     keys = each['twitter'].keys()
-        #     print keys
- 
         tweet_list.append(twitter)
            
     return tweet_list
 
+def saveObjectToOutput(fileName, data):
 
-def saveObjectToFile(fileName, data):
     with open(fileName, 'a') as outfile:
         outfile.write("{}\n".format(json.dumps(data)))
-        
-    
+
+def extractTweetfromMix(path, fileName):
+
+    file_counter = 0
+    tweet_counter = 0
+
+    for eachfile in os.listdir(path):
+        if not eachfile.startswith('.'):               
+            file_counter += 1                   
+            sub_path = path + eachfile                    
+            if 'json.part' in sub_path:
+                continue                                          
+            records = export_DataSift_file(sub_path)
+            
+            for tweet in records:
+                tweet_counter += 1
+
+                saveObjectToOutput(fileName, tweet)
+
+    print 'How many files in the folder: ' + str(file_counter)
+    print 'How many tweets in total: ' + str(tweet_counter)
+
+def extractTweetfromMonth(path, fileName):
+
+    file_counter = 0
+    tweet_counter = 0
+
+    for root, dirs, files in os.walk(path):
+        for eachfile in files:
+            if not eachfile.startswith("."):
+                file_counter += 1
+                sub_path = os.path.join(root, eachfile)
+                if "json.part" in sub_path:
+                    continue
+                records = export_DataSift_file(sub_path)
+
+                for tweet in records:
+                    tweet_counter += 1
+
+                    saveObjectToOutput(fileName, tweet)
+            
+    print 'How many files in the folder: ' + str(file_counter)
+    print 'How many tweets in total: ' + str(tweet_counter)
+
 def main():
 
     # mark the beginning time of process
     start = timeit.default_timer()
 
-    # target folder which stores every 4-week data
-    # replace 01 with [01--24], orders and details please see EXCEL table
-    path = 'RW_data/'
-
+    # path = 'RW_data_mix/'
+    path = 'RW_data_month/'
     print 'Now processing DataSift files in: ' + path
               
-    file_counter = 0
-    tweet_counter = 0
-
-    # define a destination to save Twitter data
-    fileName = 'two_months_sample.json'
+    # define a destination to save exported Twitter data
+    fileName = 'oneyear_sample.json'
             
-    for eachfile in os.listdir(path):
-        if not eachfile.startswith('.'):
-               
-            file_counter += 1
-                   
-            sub_path = path + eachfile
-                    
-            if 'json.part' in sub_path:
-                continue                        
-                  
-            records = exportDataSift(sub_path)
-            
-            for tweet in records:
-                tweet_counter += 1
-
-                saveObjectToFile(fileName, tweet)
-     
-    print 'How many files in the folder: ' + str(file_counter)
-    print 'How many tweets in total: ' + str(tweet_counter)
-    print 'Data are saved in: ' + fileName
+    extractTweetfromMonth(path, fileName)
+    print 'Done. All the data have been saved in: ' + fileName
 
     ##### mark the ending time of process #####
     end = timeit.default_timer()
@@ -80,7 +92,6 @@ def main():
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     print "This process took %d:%02d:%02d" % (h, m, s)
-
 
 if __name__ == '__main__':
     main()
