bc42ca9da5205b5642c093240d534d6187e6d157
kobe2452 <tongliu0314@gmail.com>
2015-08-23 23:13:11 -0400

    add function to count keywords in daily base, have some errors

--- count_keywords.py
+++ count_keywords.py
@@ -8,7 +8,7 @@ pLink = re.compile(r'https*:\S+\.\w+', re.IGNORECASE)
 pMention = re.compile(r'@[A-Za-z0-9_]+\b')
 pNewLine = re.compile(r'[\r\n]+')
 pRetweet = re.compile(r'\brt\b', re.IGNORECASE)
-punctuation = { 0x2018:0x27, 0x2019:0x27, 0x201C:0x22, 0x201D:0x22 }
+punctuation = {0x2018:0x27, 0x2019:0x27, 0x201C:0x22, 0x201D:0x22}
 h = HTMLParser.HTMLParser()
 
 def count_all_data_keywords(fileName, keywords):
@@ -41,10 +41,10 @@ def count_all_data_keywords(fileName, keywords):
 
                 # new_tokens = stemmer_lemmatizer(tokens)
 
-                for item in rules:
+                for index, item in enumerate(rules):
                     intersection_word = set(new_tokens).intersection(item)
                     if len(intersection_word) == len(item):
-                        keywords_dict[rules.index(item)] += 1
+                        keywords_dict[index] += 1
 
     for k, v in keywords_dict.items():
         print keywords[k], v
@@ -116,7 +116,8 @@ def get_normalized_word(word):
     if pMention.match(word):
         return '[@SOMEONE]'
     if type(word) is unicode:
-        word = word.translate(punctuation).encode('ascii', 'ignore')  # find ASCII equivalents for unicode quotes
+        # word = word.translate(punctuation).encode('ascii', 'ignore')  # find ASCII equivalents for unicode quotes
+        word = word.translate(punctuation)
     if len(word) < 1:
         return None
     if word[0] == '#':
@@ -187,6 +188,56 @@ def count_tweets_unit_time_period(fileName, MONTHS, DAYS):
     for month in MONTHS:
         print month, month_dict[month]
 
+def count_daily_words_keywords(fileName, keywords, MONTHS):
+
+    rules = process_keywords(keywords)
+
+    daily_dict = dict()
+
+    for line in open(fileName, "r"):
+        tweet = json.loads(line.decode('utf-8'))
+
+        if 'lang' in tweet:                
+            language = tweet['lang']
+
+            # Only process and analyze tweets written in English
+            if language == 'en':
+                # time information
+                timestamp = tweet['created_at'].split()
+                day = timestamp[0].strip(",")
+                date = timestamp[1]
+                month = timestamp[2]
+                year = timestamp[3]
+
+                two_digit_month = '%02d' % int(MONTHS.index(month)+1)
+                eachday = year + two_digit_month + date
+
+                keywords_dict = defaultdict(int)
+
+                # text information
+                message = tweet['text']
+                # ArkTweetNLP tokenizer
+                tokens = tokenizeRawTweetText(message)
+                new_tokens = []
+                for word in tokens:
+                    normalized_word = get_normalized_word(word)
+                    new_tokens.append(normalized_word)
+
+                for index, item in enumerate(rules):
+                    intersection_word = set(new_tokens).intersection(item)
+                    if len(intersection_word) == len(item):
+                        keywords_dict[index] += 1
+
+                    daily_dict[eachday][index] += keywords_dict[index]
+
+    # print len(daily_dict)
+    # for k, v in daily_dict.items():
+    #     print k, len(v)
+    #     for k1, v1 in keywords_dict.items():
+    #         print keywords[k1], v1
+
+    return daily_dict
+
 def main():
     # mark the beginning time of process
     start = timeit.default_timer()
@@ -199,11 +250,13 @@ def main():
 
     keywords = ['suicide', 'depression', 'media guideline', 'seek help', 'suicide lifeline', 'crisis hotline', 'Parkinson\'s', 'Robin Williams']
 
-    keywords_dict = count_all_data_keywords(fileName, keywords)
+    # keywords_dict = count_all_data_keywords(fileName, keywords)
 
-    before_dict, after_dict = split_tweets_before_after_event(fileName, EVENT, MONTHS)
+    # before_dict, after_dict = split_tweets_before_after_event(fileName, EVENT, MONTHS)
 
-    count_tweets_unit_time_period(fileName, MONTHS, DAYS)
+    # count_tweets_unit_time_period(fileName, MONTHS, DAYS)
+
+    count_daily_words_keywords(fileName, keywords, MONTHS)
 
     ##### mark the ending time of process #####
     end = timeit.default_timer()
